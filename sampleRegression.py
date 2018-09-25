b06f4d4cfb744dd89c205095d99993e2bdca13d5
richard.feng <richard.feng@nrscorporation.com>
2018-09-25 14:22:52 -0500

    python example to reference

---
+++ sampleRegression.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_boston
+from sklearn.ensemble import RandomForestRegressor
+
+boston = load_boston()
+boston.keys()
+type(boston['data'])
+print(boston['DESCR'])
+
+
+clf = RandomForestRegressor()
+
+#Training our algorithm to predict "target" from "data", features vs labels
+clf.fit(boston['data'], boston['target'])
+
+
+#See how accurate our model is
+clf.score(boston['data'], boston['target'])
+
+
+# In[16]:
+
+
+get_ipython().run_line_magic('pinfo', 'clf.score')
